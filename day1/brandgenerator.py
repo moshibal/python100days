f7ae02b406f3097ce33ced8dcb58b6bac0ba2fa7
Bishal karki <bishalcarki2020@gmail.com>
2024-02-18 22:21:19 +1100

    brand generator

---
+++ day1/brandgenerator.py
@@ -0,0 +1,4 @@
+city=input("type the name of your city!\n")
+pet=input("type the name of your pet!\n")
+brand=f"{city} {pet}"
+print(f"the name of your brand is {brand}")
---
+++ day1/main.py
@@ -0,0 +1,7 @@
+a=input()
+b=input()
+c=b
+b=a
+a=c
+print(f"a: {a}")
+print(f"b: {b}")
